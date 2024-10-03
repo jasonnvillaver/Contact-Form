7f0d66896231b166786633ea453717668c587cd5
jasonnvillaver <153735760+jasonnvillaver@users.noreply.github.com>
2024-10-03 08:53:27 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@
     
     document.getElementById('contact-form').addEventListener('submit', function(event) {
         event.preventDefault();
-        // Send the email using the form data
+        
         console.log(this)
         emailjs.sendForm(service_vx29a4t, template_53slcp5, this)
             .then(function() {
