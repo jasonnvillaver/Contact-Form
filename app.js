d84d9c91b24610afa103d1151df775610891c870
jasonnvillaver <153735760+jasonnvillaver@users.noreply.github.com>
2024-10-03 08:40:18 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+     
+     (function(){
+        emailjs.init("iiGwi6VgI5wyjN2tb");
+    })();
+ 
+ 
+    
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        // Send the email using the form data
+        console.log(this)
+        emailjs.sendForm(service_vx29a4t, template_53slcp5, this)
+            .then(function() {
+                alert('Message Sent Successfully!');
+            }, function(error) {
+                alert('Failed to send the message: ' + JSON.stringify(error));
+            });
+    });
+ 
